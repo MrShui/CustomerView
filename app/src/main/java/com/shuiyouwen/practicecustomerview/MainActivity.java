a2529431eea1e74f74af5b047da6ff729a0c8b9b
水友文 <androidforwork@163.com>
2017-07-06 14:52:26 +0800

    初始提交

---
+++ app/src/main/java/com/shuiyouwen/practicecustomerview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shuiyouwen.practicecustomerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
